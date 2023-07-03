a7314dd28f8ca1ef1b470e925cce54a8c2ba0245
Marcus Gonçalves <nvrrox@gmail.com>
2023-07-02 22:05:59 -0300

    feat: project struts

--- devops-tools/nap/cmd/cbrouter.go
+++ devops-tools/nap/cmd/cbrouter.go
@@ -1,1 +1,10 @@
 package nap
+
+import "net/http"
+
+type RouterFunc func(client *http.Client, content interface{})
+
+type CBRouter struct {
+	Routers       map[int]RouterFunc
+	DefaultRouter RouterFunc
+}
