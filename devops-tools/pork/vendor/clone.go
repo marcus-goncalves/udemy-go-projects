fbff3db9c1314872eb0bf8cd5ddad144b21f9510
Marcus Eugenio Gomes Goncalves <marcus.goncalves@mundiale.com.br>
2023-07-04 15:51:16 -0300

    feat: clone with mocks

--- devops-tools/pork/vendor/clone.go
+++ devops-tools/pork/vendor/clone.go
@@ -1,1 +1,34 @@
 package pork
+
+import (
+	"log"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	CloneCmd = &cobra.Command{
+		Use:   "clone",
+		Short: "clone a github repository",
+		Run: func(cmd *cobra.Command, args []string) {
+			if len(args) <= 0 {
+				log.Fatalln("you must provide a repository url")
+			}
+
+			if err := CloneRepository(args[0], ref, create); err != nil {
+				log.Fatalln("error cloning repo:", err)
+			}
+		},
+	}
+	ref    string
+	create bool
+)
+
+func CloneRepository(repository string, ref string, shouldCreate bool) error {
+	return nil
+}
+
+func init() {
+	CloneCmd.PersistentFlags().StringVar(&ref, "ref", "", "specific reference to check out")
+	CloneCmd.PersistentFlags().BoolVar(&create, "create", false, "create the reference if it doesn't exists")
+}
--- devops-tools/pork/vendor/clone_test.go
+++ devops-tools/pork/vendor/clone_test.go
@@ -1,1 +1,9 @@
 package pork
+
+import "testing"
+
+func TestCloneRepository(t *testing.T) {
+	if err := CloneRepository("testing", "", false); err != nil {
+		t.Fail()
+	}
+}
